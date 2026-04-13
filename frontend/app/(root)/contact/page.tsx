15a566dbafecd05e0cdb24a146d013a098da1ab7
Smirkyea <smirkyea@gmail.com>
2026-04-13 19:55:13 +0200

    chore(contact): add contact page

--- frontend/app/(root)/contact/page.tsx
+++ frontend/app/(root)/contact/page.tsx
@@ -1,11 +1,41 @@
+import PageContainer from "@/components/page/PageContainer";
+import { contactList } from "@/constants/contact";
+import Link from "next/link";
+
 export const metadata = {
   title: "Contact",
 };
 
-const page = () => {
+const Page = () => {
   return (
-    <div>page</div>
+    <PageContainer title="Contact Mineplex" childrenClassName="text-[20px] font-roboto font-light space-y-8">
+      {contactList.map((c, idx) => (
+        <div className="space-y-2" key={idx}>
+          <b className="block font-medium">
+            {c.title}:
+          </b>
+
+          <ul className="ml-7 list-disc">
+            {c.contact.map((contact, idz) => (
+              <li key={idz}>
+                <div className="flex items-center gap-1.5">
+                  {contact.label && (
+                    <p className="font-normal">
+                      {contact.label}:
+                    </p>
+                  )}
+
+                  <Link href={contact.link.href} target="_blank" className="text-amber-600 link-tertiary hover:text-amber-700">
+                    {contact.link.label}
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </PageContainer>
   )
 }
 
-export default page
+export default Page
---
+++ frontend/constants/contact.ts
@@ -0,0 +1,80 @@
+export const contactList = [
+    {
+        title: "Contact Email Addresses",
+        contact: [
+            {
+                label: null,
+                link: {
+                    label: "leadership@mineplex.com",
+                    href: "mailto:leadership@mineplex.com"
+                }
+            },
+            {
+                label: null,
+                link: {
+                    label: "developers@mineplex.com",
+                    href: "mailto:developers@mineplex.com"
+                }
+            },
+            {
+                label: null,
+                link: {
+                    label: "support@mineplex.com",
+                    href: "mailto:support@mineplex.com"
+                }
+            }
+        ]
+    },
+    {
+        title: "Social Media",
+        contact: [
+            {
+                label: "X",
+                link: {
+                    label: "@mineplex",
+                    href: "https://x.com/mineplex"
+                }
+            },
+            {
+                label: "Facebook",
+                link: {
+                    label: "MineplexGames",
+                    href: "https://www.facebook.com/MineplexGames"
+                }
+            },
+            {
+                label: "Discord",
+                link: {
+                    label: "discord.mineplex.com",
+                    href: "https://discord.mineplex.com"
+                }
+            },
+            {
+                label: "Twitch",
+                link: {
+                    label: "mineplex",
+                    href: "https://www.twitch.tv/mineplex"
+                }
+            },
+            {
+                label: "YouTube",
+                link: {
+                    label: "MineplexGamesOfficial",
+                    href: "https://www.youtube.com/c/MineplexGamesOfficial"
+                }
+            },
+        ]
+    },
+    {
+        title: "Support Pages",
+        contact: [
+            {
+                label: "Tickets Panel",
+                link: {
+                    label: "tickets.mineplex.com",
+                    href: "https://tickets.mineplex.com"
+                }
+            }
+        ]
+    }
+]
